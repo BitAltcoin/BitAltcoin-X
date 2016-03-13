ab18ba4b037732aa6b98f7579ea25d244adce38e
BitAltcoin <bitaltcoin@cloudvault.ch>
2016-03-13 22:12:34 +0100

    Update main.h

--- src/main.h
+++ src/main.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2009-2015 Satoshi Nakamoto
 // Copyright (c) 2009-2015 The Bitcoin developers
-// Copyright (c) 2015 The FlyCoin developers
+// Copyright (c) 2015 The BitAltcoin-X developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #ifndef BITCOIN_MAIN_H
@@ -585,7 +585,7 @@ public:
 
     bool IsCoinStake() const
     {
-        // FlyCoin: the coin stake transaction is marked with the first output empty
+        // BitAltcoin-X: the coin stake transaction is marked with the first output empty
         return (vin.size() > 0 && (!vin[0].prevout.IsNull()) && vout.size() >= 2 && vout[0].IsEmpty());
     }
 
@@ -771,7 +771,7 @@ public:
     bool ClientConnectInputs();
     bool CheckTransaction() const;
     bool AcceptToMemoryPool(CTxDB& txdb, bool fCheckInputs=true, bool* pfMissingInputs=NULL);
-    bool GetCoinAge(CTxDB& txdb, uint64_t& nCoinAge) const;  // FlyCoin: get transaction coin age
+    bool GetCoinAge(CTxDB& txdb, uint64_t& nCoinAge) const;  // BitAltcoin-X: get transaction coin age
 
 protected:
     const CTxOut& GetOutputFor(const CTxIn& input, const MapPrevTx& inputs) const;
@@ -923,7 +923,7 @@ public:
     // network and disk
     std::vector<CTransaction> vtx;
 
-    // FlyCoin: block signature - signed by one of the coin base txout[N]'s owner
+    // BitAltcoin-X: block signature - signed by one of the coin base txout[N]'s owner
     std::vector<unsigned char> vchBlockSig;
 
     // memory only
@@ -1002,7 +1002,7 @@ public:
         return nEntropyBit;
     }
 
-    // FlyCoin: two types of block: proof-of-work or proof-of-stake
+    // BitAltcoin-X: two types of block: proof-of-work or proof-of-stake
     bool IsProofOfStake() const
     {
         return (vtx.size() > 1 && vtx[1].IsCoinStake());
@@ -1018,7 +1018,7 @@ public:
         return IsProofOfStake()? std::make_pair(vtx[1].vin[0].prevout, vtx[1].nTime) : std::make_pair(COutPoint(), (unsigned int)0);
     }
 
-    // FlyCoin: get max transaction timestamp
+    // BitAltcoin-X: get max transaction timestamp
     int64_t GetMaxTransactionTime() const
     {
         int64_t maxTransactionTime = 0;
@@ -1161,7 +1161,7 @@ public:
     bool AddToBlockIndex(unsigned int nFile, unsigned int nBlockPos, const uint256& hashProofOfStake);
     bool CheckBlock(bool fCheckPOW=true, bool fCheckMerkleRoot=true, bool fCheckSig=true) const;
     bool AcceptBlock();
-    bool GetCoinAge(uint64_t& nCoinAge) const; // FlyCoin: calculate total coin age spent in block
+    bool GetCoinAge(uint64_t& nCoinAge) const; // BitAltcoin-X: calculate total coin age spent in block
     bool SignBlock(CWallet& keystore, int64_t nFees);
     bool CheckBlockSignature() const;
 
@@ -1189,7 +1189,7 @@ public:
     CBlockIndex* pnext;
     unsigned int nFile;
     unsigned int nBlockPos;
-    uint256 nChainTrust; // FlyCoin: trust score of block chain
+    uint256 nChainTrust; // BitAltcoin-X: trust score of block chain
     int nHeight;
 
     int64_t nMint;
@@ -1197,7 +1197,7 @@ public:
     // 39otrebla
     //int64_t nMoneyBurned;
 
-    unsigned int nFlags;  // FlyCoin: block index flags
+    unsigned int nFlags;  // BitAltcoin-X: block index flags
     enum  
     {
         BLOCK_PROOF_OF_STAKE = (1 << 0), // is proof-of-stake block
