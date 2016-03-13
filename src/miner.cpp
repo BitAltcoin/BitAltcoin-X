9df365e39ef42cf3d73f4a8fdbb87f1575498a99
BitAltcoin <bitaltcoin@cloudvault.ch>
2016-03-13 23:46:56 +0100

    Update miner.cpp

--- src/miner.cpp
+++ src/miner.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2009-2015 Satoshi Nakamoto
 // Copyright (c) 2009-2015 The Bitcoin developers
-// Copyright (c) 2015 The FlyCoin developers
+// Copyright (c) 2015 The BitAltcoin-X developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -522,7 +522,7 @@ void StakeMiner(CWallet *pwallet)
     SetThreadPriority(THREAD_PRIORITY_LOWEST);
 
     // Make this thread recognisable as the mining thread
-    RenameThread("FlyCoin-miner");
+    RenameThread("BitAltcoin-X-miner");
 
     bool fTryToSync = true;
 
