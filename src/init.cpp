c7a84f2db432e9801b168c7e11229e062a007d32
BitAltcoin <bitaltcoin@cloudvault.ch>
2016-03-13 22:15:03 +0100

    Update init.cpp

--- src/init.cpp
+++ src/init.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2009-2015 Satoshi Nakamoto
 // Copyright (c) 2009-2015 The Bitcoin developers
-// Copyright (c) 2015 The FlyCoin developers
+// Copyright (c) 2015 The BitAltcoin-X developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #include "txdb.h"
@@ -69,7 +69,7 @@ void Shutdown(void* parg)
     static bool fTaken;
 
     // Make this thread recognisable as the shutdown thread
-    RenameThread("FlyCoin-shutoff");
+    RenameThread("BitAltcoin-X-shutoff");
 
     bool fFirstThread = false;
     {
@@ -94,7 +94,7 @@ void Shutdown(void* parg)
         delete pwalletMain;
         NewThread(ExitTimeout, NULL);
         MilliSleep(50);
-        printf("FlyCoin exited\n\n");
+        printf("BitAltcoin-X exited\n\n");
         fExit = true;
 #ifndef QT_GUI
         // ensure non-UI client gets exited here, but let Bitcoin-Qt reach 'return 0;' in bitcoin.cpp
@@ -150,12 +150,12 @@ bool AppInit(int argc, char* argv[])
         if (mapArgs.count("-?") || mapArgs.count("--help"))
         {
             // First part of help message is specific to bitcoind / RPC client
-            std::string strUsage = _("FlyCoin version") + " " + FormatFullVersion() + "\n\n" +
+            std::string strUsage = _("BitAltcoin-X version") + " " + FormatFullVersion() + "\n\n" +
                 _("Usage:") + "\n" +
-                  "  FlyCoind [options]                     " + "\n" +
-                  "  FlyCoind [options] <command> [params]  " + _("Send command to -server or FlyCoind") + "\n" +
-                  "  FlyCoind [options] help                " + _("List commands") + "\n" +
-                  "  FlyCoind [options] help <command>      " + _("Get help for a command") + "\n";
+                  "  BitAltcoin-Xd [options]                     " + "\n" +
+                  "  BitAltcoin-Xd [options] <command> [params]  " + _("Send command to -server or BitAltcoin-Xd") + "\n" +
+                  "  BitAltcoin-Xd [options] help                " + _("List commands") + "\n" +
+                  "  BitAltcoin-Xd [options] help <command>      " + _("Get help for a command") + "\n";
 
             strUsage += "\n" + HelpMessage();
 
@@ -165,7 +165,7 @@ bool AppInit(int argc, char* argv[])
 
         // Command-line RPC
         for (int i = 1; i < argc; i++)
-            if (!IsSwitchChar(argv[i][0]) && !boost::algorithm::istarts_with(argv[i], "FlyCoin:"))
+            if (!IsSwitchChar(argv[i][0]) && !boost::algorithm::istarts_with(argv[i], "BitAltcoin-X:"))
                 fCommandLine = true;
 
         if (fCommandLine)
@@ -205,13 +205,13 @@ int main(int argc, char* argv[])
 
 bool static InitError(const std::string &str)
 {
-    uiInterface.ThreadSafeMessageBox(str, _("FlyCoin"), CClientUIInterface::OK | CClientUIInterface::MODAL);
+    uiInterface.ThreadSafeMessageBox(str, _("BitAltcoin-X"), CClientUIInterface::OK | CClientUIInterface::MODAL);
     return false;
 }
 
 bool static InitWarning(const std::string &str)
 {
-    uiInterface.ThreadSafeMessageBox(str, _("FlyCoin"), CClientUIInterface::OK | CClientUIInterface::ICON_EXCLAMATION | CClientUIInterface::MODAL);
+    uiInterface.ThreadSafeMessageBox(str, _("BitAltcoin-X"), CClientUIInterface::OK | CClientUIInterface::ICON_EXCLAMATION | CClientUIInterface::MODAL);
     return true;
 }
 
@@ -234,8 +234,8 @@ std::string HelpMessage()
     string strUsage = _("Options:") + "\n" +
         "  -?                     " + _("This help message") + "\n" +
         "  -exchangewallet        " + _("Wallet uses non-standard addresses (exchanges only)") + "\n" +
-        "  -conf=<file>           " + _("Specify configuration file (default: FlyCoin.conf)") + "\n" +
-        "  -pid=<file>            " + _("Specify pid file (default: FlyCoind.pid)") + "\n" +
+        "  -conf=<file>           " + _("Specify configuration file (default: BitAltcoin-X.conf)") + "\n" +
+        "  -pid=<file>            " + _("Specify pid file (default: BitAltcoin-Xd.pid)") + "\n" +
         "  -datadir=<dir>         " + _("Specify data directory") + "\n" +
         "  -wallet=<dir>          " + _("Specify wallet file (within data directory)") + "\n" +
         "  -dbcache=<n>           " + _("Set database cache size in megabytes (default: 25)") + "\n" +
@@ -510,7 +510,7 @@ bool AppInit2()
     if (file) fclose(file);
     static boost::interprocess::file_lock lock(pathLockFile.string().c_str());
     if (!lock.try_lock())
-        return InitError(strprintf(_("Cannot obtain a lock on data directory %s.  FlyCoin is probably already running."), strDataDir.c_str()));
+        return InitError(strprintf(_("Cannot obtain a lock on data directory %s.  BitAltcoin-X is probably already running."), strDataDir.c_str()));
 
 #if !defined(WIN32) && !defined(QT_GUI)
     if (fDaemon)
@@ -537,7 +537,7 @@ bool AppInit2()
     if (GetBoolArg("-shrinkdebugfile", !fDebug))
         ShrinkDebugFile();
     printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-    printf("FlyCoin version %s (%s)\n", FormatFullVersion().c_str(), CLIENT_DATE.c_str());
+    printf("BitAltcoin-X version %s (%s)\n", FormatFullVersion().c_str(), CLIENT_DATE.c_str());
     printf("Using OpenSSL version %s\n", SSLeay_version(SSLEAY_VERSION));
     if (!fLogTimestamps)
         printf("Startup time: %s\n", DateTimeStrFormat("%x %H:%M:%S", GetTime()).c_str());
@@ -546,7 +546,7 @@ bool AppInit2()
     std::ostringstream strErrors;
 
     if (fDaemon)
-        fprintf(stdout, "FlyCoin server starting\n");
+        fprintf(stdout, "BitAltcoin-X server starting\n");
 
     int64_t nStart;
 
@@ -578,7 +578,7 @@ bool AppInit2()
                                      " Original wallet.dat saved as wallet.{timestamp}.bak in %s; if"
                                      " your balance or transactions are incorrect you should"
                                      " restore from a backup."), strDataDir.c_str());
-            uiInterface.ThreadSafeMessageBox(msg, _("FlyCoin"), CClientUIInterface::OK | CClientUIInterface::ICON_EXCLAMATION | CClientUIInterface::MODAL);
+            uiInterface.ThreadSafeMessageBox(msg, _("BitAltcoin-X"), CClientUIInterface::OK | CClientUIInterface::ICON_EXCLAMATION | CClientUIInterface::MODAL);
         }
         if (r == CDBEnv::RECOVER_FAIL)
             return InitError(_("wallet.dat corrupt, salvage failed"));
@@ -699,7 +699,7 @@ bool AppInit2()
         }
     }
 
-    if (mapArgs.count("-reservebalance")) // FlyCoin: reserve balance amount
+    if (mapArgs.count("-reservebalance")) // BitAltcoin-X: reserve balance amount
     {
         if (!ParseMoney(mapArgs["-reservebalance"], nReserveBalance))
         {
@@ -708,7 +708,7 @@ bool AppInit2()
         }
     }
 
-    if (mapArgs.count("-checkpointkey")) // FlyCoin: checkpoint master priv key
+    if (mapArgs.count("-checkpointkey")) // BitAltcoin-X: checkpoint master priv key
     {
         if (!Checkpoints::SetCheckpointPrivKey(GetArg("-checkpointkey", "")))
             InitError(_("Unable to sign checkpoint, wrong checkpointkey?\n"));
@@ -812,13 +812,13 @@ bool AppInit2()
         {
             string msg(_("Warning: error reading wallet.dat! All keys read correctly, but transaction data"
                          " or address book entries might be missing or incorrect."));
-            uiInterface.ThreadSafeMessageBox(msg, _("FlyCoin"), CClientUIInterface::OK | CClientUIInterface::ICON_EXCLAMATION | CClientUIInterface::MODAL);
+            uiInterface.ThreadSafeMessageBox(msg, _("BitAltcoin-X"), CClientUIInterface::OK | CClientUIInterface::ICON_EXCLAMATION | CClientUIInterface::MODAL);
         }
         else if (nLoadWalletRet == DB_TOO_NEW)
-            strErrors << _("Error loading wallet.dat: Wallet requires newer version of FlyCoin") << "\n";
+            strErrors << _("Error loading wallet.dat: Wallet requires newer version of BitAltcoin-X") << "\n";
         else if (nLoadWalletRet == DB_NEED_REWRITE)
         {
-            strErrors << _("Wallet needed to be rewritten: restart FlyCoin to complete") << "\n";
+            strErrors << _("Wallet needed to be rewritten: restart BitAltcoin-X to complete") << "\n";
             printf("%s", strErrors.str().c_str());
             return InitError(strErrors.str());
         }
